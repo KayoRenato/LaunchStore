d375b2f58d2b03ac4d3331b6346a8ec33694136f
Kayo Renato <kayorenatocontato@gmail.com>
2020-02-19 08:57:25 -0300

    Initial project configuration

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ const routes = require('./routes')
 const methodOverride = require('method-override')
 
 
-const server = express() //criar o servidor 
+const server = express() 
 
 server.use(express.urlencoded({extended:true}))
-server.use(express.static('public')) //utilizar arquivos staticos (style.css)
-server.use(methodOverride('_method')) // sobrescrever as rodas com PUT e DELETE
-server.use(routes) //disponibilizar as rotas para o servidor
+server.use(express.static('public')) 
+server.use(methodOverride('_method')) 
+server.use(routes)
 
 server.set("view engine", "njk")
 
